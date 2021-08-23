a97ab5a7d4f6a0aced6de106b8613af568aaa106
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-23 18:11:32 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Suppose! If it _____ last weekend, we _____ to play tennis.",
+        options:["rained / wouldn’t be able", "rains / won’t be able", "rain / would be able", "had rained - could"],
+        answer: 0
+    },
+    {
+        q:"Give me Peter’s letter. If I _____ him, I _____ it to him.",
+        options:["saw / would give", "see / will give", "meet / would visit", "had seen / would give"],
+        answer: 1
+    },
+    {
+        q:"I have to work about 80 hours a week, so I’m very busy. But if I _____ any spare time, I _____ a sport like golf.",
+        options:["will have / will take up", "had / will take up", "had / would take up", "will have / had"],
+        answer: 2
+    },
+    {
+        q:"If I _____ taller, I _____ be a policeman, but I’m too short.",
+        options:["was / can", "had / could", "am / will", "were / could"],
+        answer: 3
+    },
+    {
+        q:"Please, start your meal. If you _____ your soup now, it _____ cold.",
+        options:["don’t have / will go", "can eat / doesn’t go", "had / got", "didn’t have / would go"],
+        answer: 0
+    },
+    {
+        q:"What noisy neighbours you’ve got! If my neighbours _____ as bad as yours, I _____ crazy.",
+        options:["are / will go", "were / would go", "are / would go", "had been / would have gone"],
+        answer: 1
+    },
+    {
+        q:"If you _____ any problems, let me know and I _____ and help you straight away.",
+        options:["had had / would come", "had / would come", "have / will come", "have / would go"],
+        answer: 2
+    },
+    {
+        q:"You’re a brilliant cook! If I _____ cook as well as you, I _____ a restaurant.",
+        options:["can / would open", "could / will open", "can / will open", "could / would open"],
+        answer: 3
+    },
+    {
+        q:"If there _____ some nice fish in the supermarket, _____ you _____ some for supper tonight?",
+        options:["are / will / buy", "is / will / buy", "were / would / buy", "had been / would / buy"],
+        answer: 0
+    },
+    {
+        q:"I’m small. I wish I _____ small.",
+        options:["were", "weren’t", "was", "am not"],
+        answer: 1
+    }
+
+
+
+]
